f7a91840b416b8125b52edef46475ad80ac456c6
Adriano Misina <adriano.misina@gmail.com>
2022-06-06 15:26:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+  //Select the elements
+  let countEl = document.getElementById("count-el");
+  let saveEl = document.getElementById("save-el");
+  let total = document.getElementById("total");
+  let count = 0;
+
+  //Functions
+
+  //Add Passenger
+  function increment() {
+    count = count + 1;
+    countEl.textContent = count;
+  }
+
+  //Save Passenger
+  function saveData() {
+    let countTotal = count + ' - ';
+    saveEl.textContent += countTotal;
+    countEl.textContent = 0;
+    count = 0; 
+    totalAcumulado()
+  }
+
+
